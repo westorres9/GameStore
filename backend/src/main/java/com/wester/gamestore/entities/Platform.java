5bc4d03bf3d3fe75e9d549a407ee674430111ee2
westorres9 <wester.torres9@gmail.com>
2022-05-04 17:17:39 -0300

    implementado classe Plataforma

---
+++ backend/src/main/java/com/wester/gamestore/entities/Platform.java
@@ -0,0 +1,55 @@
+package com.wester.gamestore.entities;
+
+import java.util.Objects;
+
+public class Platform {
+	
+	private Long id;
+	private String name;
+	
+	public Platform() {
+	}
+
+	public Platform(Long id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Platform other = (Platform) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+	
+	
+
+}
